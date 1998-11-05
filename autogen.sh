557c14ce8add363d4b022cb715f69b00a62c033b
James Henstridge <james@daa.com.au>
1998-11-05 11:31:45 +0000

    added a simple autogen shell script to help with CVS builds.
    1998-11-05  James Henstridge  <james@daa.com.au>
            * autogen.sh: added a simple autogen shell script to help with CVS
            builds.

---
+++ autogen.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+DIE=0
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have autoconf installed to compile gnome-xml."
+	echo "Download the appropriate package for your distribution,"
+	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+	DIE=1
+}
+
+(libtool --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have libtool installed to compile gnome-xml."
+	echo "Get ftp://alpha.gnu.org/gnu/libtool-1.2.tar.gz"
+	echo "(or a newer version if it is available)"
+	DIE=1
+}
+
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have automake installed to compile gnome-xml."
+	echo "Get ftp://ftp.cygnus.com/pub/home/tromey/automake-1.2d.tar.gz"
+	echo "(or a newer version if it is available)"
+	DIE=1
+}
+
+if test "$DIE" -eq 1; then
+	exit 1
+fi
+
+test -f test-libglade.c || {
+	echo "You must run this script in the top-level libglade directory"
+	exit 1
+}
+
+if test -z "$*"; then
+	echo "I am going to run ./configure with no arguments - if you wish "
+        echo "to pass any to it, please specify them on the $0 command line."
+fi
+
+libtoolize --copy --force
+aclocal $ACLOCAL_FLAGS
+automake --add-missing
+autoconf
+./configure "$@"
+
+echo 
+echo "Now type 'make' to compile libglade."
