15bb22dbe5f7422700e904f5ac2eee44856703db
James Henstridge <jamesh@src.gnome.org>
2002-09-03 00:24:23 +0000

    change message if automake-1.6 is not installed.

--- autogen.sh
+++ autogen.sh
@@ -52,8 +52,8 @@ if automake-1.6 --version < /dev/null > /dev/null 2>&1 ; then
 fi
 if $have_automake ; then : ; else
 	echo
-	echo "You must have automake 1.4-p1 installed to compile $PROJECT."
-	echo "Get ftp://ftp.gnu.org/pub/gnu/automake/automake-1.4-p1.tar.gz"
+	echo "You must have automake 1.6.x installed to compile $PROJECT."
+	echo "Get ftp://ftp.gnu.org/pub/gnu/automake/automake-1.6.3.tar.gz"
 	echo "(or a newer version if it is available)"
 	DIE=1
 fi
